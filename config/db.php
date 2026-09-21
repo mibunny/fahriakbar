e055852c1655c543517fcc2c0523336577f6d9ab
Muhammad Fachri <mhd.fachrii30@gmail.com>
2026-09-21 16:50:09 +0700

    Update database configuration for hosting

--- config/db.php
+++ config/db.php
@@ -1,10 +1,17 @@
 <?php
-$host = "localhost";
-$user = "root";
+
+$host = "sql303.infinityfree.com";
+$user = "if0_42971248";
 $pass = "";
-$db   = "bansos";
+$db   = "if0_42971248_bansos";
 
 $conn = new mysqli($host, $user, $pass, $db);
+
 if ($conn->connect_error) {
-    die(json_encode(['status' => 'error', 'msg' => 'Koneksi gagal: ' . $conn->connect_error]));
+    die(json_encode([
+        'status' => 'error',
+        'msg' => 'Koneksi gagal: ' . $conn->connect_error
+    ]));
 }
+
+$conn->set_charset("utf8mb4");
