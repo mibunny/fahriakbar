a4a9b2edd31a602cdf94b073fd907e8dc955248c
mibunny <64744016+mibunny@users.noreply.github.com>
2026-09-21 16:51:37 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,17 +1,10 @@
 <?php
-
-$host = "sql303.infinityfree.com";
-$user = "if0_42971248";
-$pass = "nalerori02";
-$db   = "if0_42971248_bansos";
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "bansos";
 
 $conn = new mysqli($host, $user, $pass, $db);
-
 if ($conn->connect_error) {
-    die(json_encode([
-        'status' => 'error',
-        'msg' => 'Koneksi gagal: ' . $conn->connect_error
-    ]));
+    die(json_encode(['status' => 'error', 'msg' => 'Koneksi gagal: ' . $conn->connect_error]));
 }
-
-$conn->set_charset("utf8mb4");
